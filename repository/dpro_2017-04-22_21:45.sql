7fa8c1a4eb762206e3ed0eaec2f5b0db65604ea0
Tomasz Truszkowski <tomasz.truszkowski2013@gmail.com>
2017-04-22 21:43:27 +0200

    Database tables names change.

--- repository/dpro_2017-04-22_21:45.sql
+++ repository/dpro_2017-04-22_21:45.sql
@@ -4,9 +4,7 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
--- -----------------------------------------------------
--- Schema dpro
--- -----------------------------------------------------
+DROP DATABASE IF EXISTS `dpro`;
 
 CREATE DATABASE IF NOT EXISTS `dpro` DEFAULT CHARACTER SET utf8;
 
@@ -31,9 +29,9 @@ INSERT INTO `dpro`.`roles` (`user_name`, `role`) VALUES ('student', 'ROLE_STUDEN
 
 
 -- -----------------------------------------------------
--- Table `dpro`.`user`
+-- Table `dpro`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dpro`.`user` (
+CREATE TABLE IF NOT EXISTS `dpro`.`users` (
   `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_name` VARCHAR(255) NOT NULL,
   `user_password` VARCHAR(255) NOT NULL,
@@ -53,14 +51,14 @@ CREATE TABLE IF NOT EXISTS `dpro`.`user` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-INSERT INTO `dpro`.`user` (`user_name`, `user_password`, `user_first_name`, `user_last_name`, `user_enabled`, `user_email`, `user_date_of_birth`, `user_pesel`) VALUES ('instructor', '123', 'Stachu', 'Jones', true, 'instructor@gmail.com', '1955-11-23', '55112323476');
+INSERT INTO `dpro`.`users` (`user_name`, `user_password`, `user_first_name`, `user_last_name`, `user_enabled`, `user_email`, `user_date_of_birth`, `user_pesel`) VALUES ('instructor', '123', 'Stachu', 'Jones', true, 'instructor@gmail.com', '1955-11-23', '55112323476');
 
-INSERT INTO `dpro`.`user` (`user_name`, `user_password`, `user_first_name`, `user_last_name`, `user_enabled`, `user_email`, `user_date_of_birth`, `user_pesel`) VALUES ('student', '123', 'Tomasz', 'Truszkowski', true, 'student@gmail.com', '1991-11-01', '91011102519');
+INSERT INTO `dpro`.`users` (`user_name`, `user_password`, `user_first_name`, `user_last_name`, `user_enabled`, `user_email`, `user_date_of_birth`, `user_pesel`) VALUES ('student', '123', 'Tomasz', 'Truszkowski', true, 'student@gmail.com', '1991-11-01', '91011102519');
 
 -- -----------------------------------------------------
--- Table `dpro`.`student`
+-- Table `dpro`.`students`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dpro`.`student` (
+CREATE TABLE IF NOT EXISTS `dpro`.`students` (
   `student_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `student_album` VARCHAR(255) NOT NULL,
   `user_id` INT UNSIGNED NOT NULL,
@@ -69,18 +67,18 @@ CREATE TABLE IF NOT EXISTS `dpro`.`student` (
   INDEX `fk_student_user_idx` (`user_id` ASC),
   CONSTRAINT `fk_student_user`
     FOREIGN KEY (`user_id`)
-    REFERENCES `dpro`.`user` (`user_id`)
+    REFERENCES `dpro`.`users` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-INSERT INTO `dpro`.`student` (`student_album`, `user_id`) VALUES ('81915', 2);
+INSERT INTO `dpro`.`students` (`student_album`, `user_id`) VALUES ('81915', 2);
 
 
 -- -----------------------------------------------------
--- Table `dpro`.`instructor`
+-- Table `dpro`.`instructors`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dpro`.`instructor` (
+CREATE TABLE IF NOT EXISTS `dpro`.`instructors` (
   `instructor_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` INT UNSIGNED NOT NULL,
   `instructor_science_degree` VARCHAR(255) NULL,
@@ -88,31 +86,31 @@ CREATE TABLE IF NOT EXISTS `dpro`.`instructor` (
   INDEX `fk_instructor_user_idx` (`user_id` ASC),
   CONSTRAINT `fk_instructor_user`
     FOREIGN KEY (`user_id`)
-    REFERENCES `dpro`.`user` (`user_id`)
+    REFERENCES `dpro`.`users` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-INSERT INTO `dpro`.`instructor` (`instructor_science_degree`, `user_id`) VALUES ('Doktor', 1);
+INSERT INTO `dpro`.`instructors` (`instructor_science_degree`, `user_id`) VALUES ('Doktor', 1);
 
 
 -- -----------------------------------------------------
--- Table `dpro`.`subject_type`
+-- Table `dpro`.`subject_types`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dpro`.`subject_type` (
+CREATE TABLE IF NOT EXISTS `dpro`.`subject_types` (
   `subject_type_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `subject_type_name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`subject_type_id`))
 ENGINE = InnoDB;
 
-INSERT INTO `dpro`.`subject_type` (`subject_type_name`) VALUES ('Wykład');
-INSERT INTO `dpro`.`subject_type` (`subject_type_name`) VALUES ('Ćwiczenia');
-INSERT INTO `dpro`.`subject_type` (`subject_type_name`) VALUES ('Laboratorium');
+INSERT INTO `dpro`.`subject_types` (`subject_type_name`) VALUES ('Wykład');
+INSERT INTO `dpro`.`subject_types` (`subject_type_name`) VALUES ('Ćwiczenia');
+INSERT INTO `dpro`.`subject_types` (`subject_type_name`) VALUES ('Laboratorium');
 
 -- -----------------------------------------------------
--- Table `dpro`.`subject`
+-- Table `dpro`.`subjects`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dpro`.`subject` (
+CREATE TABLE IF NOT EXISTS `dpro`.`subjects` (
   `subject_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `subject_name` VARCHAR(255) NOT NULL,
   `subject_ects` INT UNSIGNED NOT NULL,
@@ -122,45 +120,45 @@ CREATE TABLE IF NOT EXISTS `dpro`.`subject` (
   INDEX `fk_subject_subject_type_idx` (`subject_type_id` ASC),
   CONSTRAINT `fk_subject_subject_type`
     FOREIGN KEY (`subject_type_id`)
-    REFERENCES `dpro`.`subject_type` (`subject_type_id`)
+    REFERENCES `dpro`.`subject_types` (`subject_type_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-INSERT INTO `dpro`.`subject` (`subject_name`, `subject_ects`, `subject_hours`, `subject_type_id`) VALUES ('Matematyka Dyskretna', 5, 30, 1);
-INSERT INTO `dpro`.`subject` (`subject_name`, `subject_ects`, `subject_hours`, `subject_type_id`) VALUES ('Matematyka Dyskretna', 5, 35, 2);
+INSERT INTO `dpro`.`subjects` (`subject_name`, `subject_ects`, `subject_hours`, `subject_type_id`) VALUES ('Matematyka Dyskretna', 5, 30, 1);
+INSERT INTO `dpro`.`subjects` (`subject_name`, `subject_ects`, `subject_hours`, `subject_type_id`) VALUES ('Matematyka Dyskretna', 5, 35, 2);
 
 
 -- -----------------------------------------------------
--- Table `dpro`.`user_subject`
+-- Table `dpro`.`user_subjects`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dpro`.`user_subject` (
+CREATE TABLE IF NOT EXISTS `dpro`.`user_subjects` (
   `user_id` INT UNSIGNED NOT NULL,
   `subject_id` INT UNSIGNED NOT NULL,
   INDEX `fk_user_subject_user1_idx` (`user_id` ASC),
   INDEX `fk_user_subject_subject1_idx` (`subject_id` ASC),
   CONSTRAINT `fk_user_subject_user1`
     FOREIGN KEY (`user_id`)
-    REFERENCES `dpro`.`user` (`user_id`)
+    REFERENCES `dpro`.`users` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_user_subject_subject1`
     FOREIGN KEY (`subject_id`)
-    REFERENCES `dpro`.`subject` (`subject_id`)
+    REFERENCES `dpro`.`subjects` (`subject_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-INSERT INTO `dpro`.`user_subject` (`user_id`, `subject_id`) VALUES (1, 1);
-INSERT INTO `dpro`.`user_subject` (`user_id`, `subject_id`) VALUES (1, 2);
-INSERT INTO `dpro`.`user_subject` (`user_id`, `subject_id`) VALUES (2, 1);
-INSERT INTO `dpro`.`user_subject` (`user_id`, `subject_id`) VALUES (2, 2);
+INSERT INTO `dpro`.`user_subjects` (`user_id`, `subject_id`) VALUES (1, 1);
+INSERT INTO `dpro`.`user_subjects` (`user_id`, `subject_id`) VALUES (1, 2);
+INSERT INTO `dpro`.`user_subjects` (`user_id`, `subject_id`) VALUES (2, 1);
+INSERT INTO `dpro`.`user_subjects` (`user_id`, `subject_id`) VALUES (2, 2);
 
 
 -- -----------------------------------------------------
--- Table `dpro`.`mark`
+-- Table `dpro`.`marks`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dpro`.`mark` (
+CREATE TABLE IF NOT EXISTS `dpro`.`marks` (
   `mark_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `mark_value` FLOAT(3,2) UNSIGNED NOT NULL,
   `mark_desc` VARCHAR(255) NOT NULL,
@@ -169,12 +167,12 @@ CREATE TABLE IF NOT EXISTS `dpro`.`mark` (
   UNIQUE INDEX `mark_unique` (`mark_value` ASC, `mark_desc` ASC, `mark_is_pass` ASC))
 ENGINE = InnoDB;
 
-INSERT INTO `mark` (`mark_value`, `mark_desc`, `mark_is_pass`) VALUES (2.0, 'Niedostateczny', '0');
-INSERT INTO `mark` (`mark_value`, `mark_desc`, `mark_is_pass`) VALUES (3.0, 'Dostateczny', '1');
-INSERT INTO `mark` (`mark_value`, `mark_desc`, `mark_is_pass`) VALUES (3.5, 'Dostateczny Plus', '1');
-INSERT INTO `mark` (`mark_value`, `mark_desc`, `mark_is_pass`) VALUES (4.0, 'Dobry', '1');
-INSERT INTO `mark` (`mark_value`, `mark_desc`, `mark_is_pass`) VALUES (4.5, 'Dobry Plus', '1');
-INSERT INTO `mark` (`mark_value`, `mark_desc`, `mark_is_pass`) VALUES (5.0, 'Bardzo Dobry', '1');
+INSERT INTO `marks` (`mark_value`, `mark_desc`, `mark_is_pass`) VALUES (2.0, 'Niedostateczny', '0');
+INSERT INTO `marks` (`mark_value`, `mark_desc`, `mark_is_pass`) VALUES (3.0, 'Dostateczny', '1');
+INSERT INTO `marks` (`mark_value`, `mark_desc`, `mark_is_pass`) VALUES (3.5, 'Dostateczny Plus', '1');
+INSERT INTO `marks` (`mark_value`, `mark_desc`, `mark_is_pass`) VALUES (4.0, 'Dobry', '1');
+INSERT INTO `marks` (`mark_value`, `mark_desc`, `mark_is_pass`) VALUES (4.5, 'Dobry Plus', '1');
+INSERT INTO `marks` (`mark_value`, `mark_desc`, `mark_is_pass`) VALUES (5.0, 'Bardzo Dobry', '1');
 
 -- -----------------------------------------------------
 -- Table `dpro`.`student_marks`
@@ -188,17 +186,17 @@ CREATE TABLE IF NOT EXISTS `dpro`.`student_marks` (
   INDEX `fk_student_marks_user1_idx` (`user_id` ASC),
   CONSTRAINT `fk_student_marks_subject1`
     FOREIGN KEY (`subject_id`)
-    REFERENCES `dpro`.`subject` (`subject_id`)
+    REFERENCES `dpro`.`subjects` (`subject_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_student_marks_mark1`
     FOREIGN KEY (`mark_id`)
-    REFERENCES `dpro`.`mark` (`mark_id`)
+    REFERENCES `dpro`.`marks` (`mark_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_student_marks_user1`
     FOREIGN KEY (`user_id`)
-    REFERENCES `dpro`.`user` (`user_id`)
+    REFERENCES `dpro`.`users` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
--- src/main/java/com/dpro/utils/DatabaseTables.java
+++ src/main/java/com/dpro/utils/DatabaseTables.java
@@ -6,13 +6,13 @@ package com.dpro.utils;
  */
 public class DatabaseTables {
 
-    public static final String USERS_TABLE = "user";
-    public static final String STUDENTS_TABLE = "student";
-    public static final String INSTRUCTORS_TABLE = "instructor";
+    public static final String USERS_TABLE = "users";
+    public static final String STUDENTS_TABLE = "students";
+    public static final String INSTRUCTORS_TABLE = "instructors";
     public static final String ROLES_TABLE = "roles";
-    public static final String MARKS_TABLE = "mark";
-    public static final String SUBJECTS_TABLE = "subject";
-    public static final String SUBJECT_TYPES_TABLE = "subject_type";
-    public static final String USERS_SUBJECTS_TABLE = "user_subject";
-    public static final String STUDENTS_MARKS_TABLE = "student_mark";
+    public static final String MARKS_TABLE = "marks";
+    public static final String SUBJECTS_TABLE = "subjects";
+    public static final String SUBJECT_TYPES_TABLE = "subject_types";
+    public static final String USERS_SUBJECTS_TABLE = "user_subjects";
+    public static final String STUDENTS_MARKS_TABLE = "student_marks";
 }
